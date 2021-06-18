139b987dca6fee925ff84a90ef30fa774a910a39
arunstiwari <arunstiwari@gmail.com>
2021-06-18 19:22:16 +0530

    Added test to support the sqs call to sqs mock service

--- src/test/java/com/tekmentors/springsqs/SpringSqsApplicationTests.java
+++ src/test/java/com/tekmentors/springsqs/SpringSqsApplicationTests.java
@@ -17,7 +17,7 @@ class SpringSqsApplicationTests {
 		log.info("test with message template.");
 //		for (int i = 0; i <10; i++){
 			messagePublisher.send("Test Message");
-			Thread.sleep(10000);
+//			Thread.sleep(10000);
 //		}
 
 	}
